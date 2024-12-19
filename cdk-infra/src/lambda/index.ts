92df3fa412bbd54372cc2848044086a50fe53282
Rana Rayhan <ryanrazz00@gmail.com>
2024-12-19 18:08:10 +0200

    gh

--- cdk-infra/src/lambda/index.ts
+++ cdk-infra/src/lambda/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 // Get all users
 app.get("/", async (req, res) => {
   try {
-    // await initializeDatabase();
+    await initializeDatabase();
     // const users = await AppDataSource.getRepository(User).find();
     res.json({ message: "Success" });
   } catch (error) {
